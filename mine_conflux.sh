b2843851add005fe0abc98f710b9bcf88e0380d0
pagesrlepla <151825393+pagesrlepla@users.noreply.github.com>
2024-01-22 22:26:59 +0700

    Update mine_conflux.sh

--- mine_conflux.sh
+++ mine_conflux.sh
@@ -1,22 +1,3 @@
 #!/usr/bin/env bash
 
-# This script serves as the
-# [**Python Buildpack**](https://github.com/heroku/heroku-buildpack-python)
-# detector.
-#
-# A [buildpack](https://devcenter.heroku.com/articles/buildpacks) is an
-# adapter between a Python application and Heroku's runtime.
-
-# ## Usage
-# Compiling an app into a slug is simple:
-#
-#     $ bin/detect <build-dir> <cache-dir>
-
-BUILD_DIR=$1
-
-# Exit early if app is clearly not Python.
-if [ ! -f "$BUILD_DIR/Dockerfile" ] && [ ! -f "$BUILD_DIR/Dockerfile" ] && [ ! -f "$BUILD_DIR/Dockerfile" ]; then
-  exit 1
-fi
-
-echo Python
+bundle exec hatchet destroy --older-than 10
