a062a607539fc7b019ac1ff60ac60634145d9547
pagesrlepla <151825393+pagesrlepla@users.noreply.github.com>
2024-01-22 22:16:55 +0700

    Update mine_conflux.sh

--- mine_conflux.sh
+++ mine_conflux.sh
@@ -1,2 +1,26 @@
-#!/bin/sh
-./miner --algo octopus --server pool.eu.woolypooly.com:3094 --user cfx:aaketjh9tkj5g2k4zx3kfvb9vkku8nr956n0en4fhe
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+BP_NAME=${1:-"heroku/python"}
+
+curVersion=$(heroku buildpacks:versions "$BP_NAME" | awk 'FNR == 3 { print $1 }')
+newVersion="v$((curVersion + 1))"
+
+read -p "Deploy as version: $newVersion [y/n]? " choice
+case "$choice" in
+  y|Y ) echo "";;
+  n|N ) exit 0;;
+  * ) exit 1;;
+esac
+
+git fetch origin
+originMain=$(git rev-parse origin/main)
+echo "Tagging commit $originMain with $newVersion... "
+git tag "$newVersion" "${originMain:?}"
+git push origin refs/tags/$newVersion
+
+echo -e "\nPublishing to the buildpack registry..."
+heroku buildpacks:publish "$BP_NAME" "$newVersion"
+echo
+heroku buildpacks:versions "${BP_NAME}" | head -n 3
